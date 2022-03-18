74114bd20c025f956e46080a9cb40617dbe3321f
SEbastianSpin <sebasespinalejo@gmial.com>
2022-03-18 09:24:59 +0100

    works

--- PONG/PONG.cpp
+++ PONG/PONG.cpp
@@ -240,12 +240,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             {
                 hbrush = CreateSolidBrush(cc.rgbResult);
                 rgbCurrent = cc.rgbResult;
+                SetClassLongPtr(hWnd,-10, (LONG)hbrush);  // GCLP_ HBRBACKGROUND -10
+                InvalidateRect(hWnd, NULL, TRUE);
             }
 
-
+           //
         
         }
             break;
+/*
+        case ID_BACKGROUND_BITMAP:
+
+        {
+            OPENFILENAME ofn;       // common dialog box structure
+            char szFile[260];       // buffer for file name
+                    
+            HANDLE hf;              // file handle
+
+            // Initialize OPENFILENAME
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hWnd;  // owner window
+            ofn.lpstrFile = szFile;
+            // Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+            // use the contents of szFile to initialize itself.
+            ofn.lpstrFile[0] = '\0';
+            ofn.nMaxFile = sizeof(szFile);
+            ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+            ofn.nFilterIndex = 1;
+            ofn.lpstrFileTitle = NULL;
+            ofn.nMaxFileTitle = 0;
+            ofn.lpstrInitialDir = NULL;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+            // Display the Open dialog box. 
+
+            if (GetOpenFileName(&ofn) == TRUE)
+                hf = CreateFile(ofn.lpstrFile,
+                    GENERIC_READ,
+                    0,
+                    (LPSECURITY_ATTRIBUTES)NULL,
+                    OPEN_EXISTING,
+                    FILE_ATTRIBUTE_NORMAL,
+                    (HANDLE)NULL);
+        }
+            break;
+        */
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
